afaae74570694684c237310a6b5a34d558a61b29
suriyasingaravel <suriyasingaravel710.com>
2023-12-14 12:41:39 +0530

    resume changed

--- js/main.js
+++ js/main.js
@@ -77,11 +77,11 @@ function transform_hamburger() {
 
 document.getElementById("resume-link-1").onclick = () => {
 
-    window.open("https://drive.google.com/file/d/1NoAAcjg4IxSXeE_8z6-c0rqG8Ts7jTPG/view?usp=sharing");
+    window.open("https://drive.google.com/file/d/1wo6d_ljlDk7cueY92oGBtbys47sg1141/view?usp=sharing");
 }
 document.getElementById("resume-link-2").onclick = () => {
 
-    window.open("https://drive.google.com/file/d/1NoAAcjg4IxSXeE_8z6-c0rqG8Ts7jTPG/view?usp=sharing");
+    window.open("https://drive.google.com/file/d/1wo6d_ljlDk7cueY92oGBtbys47sg1141/view?usp=sharing");
 
 }
 
